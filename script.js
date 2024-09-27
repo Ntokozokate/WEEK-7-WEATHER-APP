9ae31d4e7a6e321ba95a4ca9b64953349dd40ba4
Ntokozokate <mangenakate@gmail.com>
2024-09-27 15:00:47 +0200

    Added new functions to display data
    Changed some HTML details and added new functions to call on the API and display weather data

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-let searchFormElement = document.querySelector("#search-form");
+function refreshWeather(response) {
+  console.log(response.data.temperature.current);
+  let currentTemp = document.querySelector("#temp");
+  currentTemp.innerHTML = Math.round(response.data.temperature.current);
+  let currentHumidity = document.querySelector("#humidity");
+  currentHumidity.innerHTML = response.data.temperature.humidity;
+  let currentWindSpeed = document.querySelector("#wind-speed");
+  currentWindSpeed.innerHTML = Math.round(response.data.wind.speed);
+  console.log(response.data.wind.speed);
+}
+
+function searchCity(city) {
+  let apiKey = "3b1f6a3cf426c8fo47d42200c9911tba";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  //console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
+}
 
 function searchWeather(event) {
   event.preventDefault();
@@ -6,11 +22,8 @@ function searchWeather(event) {
   //console.log(searchInput.value);
   let cityElement = document.querySelector("#city-output");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
-searchFormElement.addEventListener("submit", searchWeather);
-//call the API
-//inject the Axios
-//call the axios
-//display the temperature
-//
+let seachFormElement = document.querySelector("#search-form");
+seachFormElement.addEventListener("submit", searchWeather);
